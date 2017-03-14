8a56765744b51812914c76986a9bee30d0ee0d1d
mark-woodrow <markwoodrow@yahoo.co.uk>
2017-03-14 21:25:35 +0000

    Tuesday evening

--- Netbeans Projects for Tutoring/Encryption test/main.c
+++ Netbeans Projects for Tutoring/Encryption test/main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.c
  * Author: mark
  *
@@ -8,26 +8,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wctype.h>
+
+
+/**
+ * safely print a string that may contain garbage
+ */
+
+void safeprintstring(char * buff) {
+
+    int length = strlen(buff);
+    // try and sanitise length
+    if (length < 0)
+        length = 1;
+    if (length > 40)
+        length = 40;
+
+    int i = 0;
+    int c;
+    while (i < length) {
+        c = buff[i];
+        //c = c && 255;
+        if ((c > 65) && (c < 255))
+            printf("%c", c);
+        else
+            printf("=");
+        i++;
+    }
+
+}
 
 /*
- * 
+ *
  */
 int main(int argc, char** argv) {
 
 
-    int mask = 128;
+    // use a mask of 2^n  -1
+    int mask = 255;
     int seed = 42;
     char message[] = "Hello, world";
+    // a buffer to put the encoded message into
     char newMessage[512];
 
+    // initialise with our seed value
     srand(seed);
 
     int i;
     int length = strlen(message);
 
+    // scan the message, xor each character with the next random number
     for (i = 0; i < length; i++) {
         newMessage[i] = message[i] ^ (random() % mask);
     }
+    // put in a terminating NULL
     newMessage[length] = 0;
 
     printf("Old message: %s\n", message);
@@ -45,15 +79,17 @@ int main(int argc, char** argv) {
     // demonstrate a brute-force attack
 
     char buffer[512];
-    for (seed = 0; seed < 255; seed++) {
+    for (seed = 0; seed < mask; seed++) {
         srand(seed);
         for (i = 0; i < length; i++) {
             buffer[i] = newMessage[i] ^ (random() % mask);
         }
         buffer[length] = 0;
-        printf("Seed: %d message: %s\n", seed, buffer);
+        printf("Using seed: %d gives message:   ", seed);
+        safeprintstring(buffer);
+        //printf("%s", buffer);
+        printf("\n");
     }
-
 
     return (EXIT_SUCCESS);
 }
